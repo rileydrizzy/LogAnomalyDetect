6f037b18246f3cfded212ef7eaaa7dab672463c6
Ladipo Ipadeola <rileydrizzy@hotmail.com>
2023-07-22 05:49:26 +0100

    backup

--- src/models/1DCNN.py
+++ src/models/1DCNN.py
@@ -1,21 +1,32 @@
-"""
-doc
+""" doc
+
 """
 
 import tensorflow as tf
 import hydra
 from omegaconf import DictConfig
 
-@hydra.main(version_base=None, config_path="conf", config_name="config")
+@hydra.main(version_base=None, config_path="conf", config_name="new")
 
-def model(cfg: DictConfig):
-    """doc
+    
+def build_model(cfg: DictConfig):
+    """ 1DCNN doc
+
+    Parameters
+    ----------
+    file_path : str
+
+    Returns
+    -------
+    model : object
+        model
     """
+
     Input_layer = tf.keras.layers.Input(shape=(1,1))
     embeding_layer = tf.keras.layers.Embedding()
-
 
     model.complie(loss = ['BCE'],
                   class_weights = ,
                   optimizer= )
+    
     return model

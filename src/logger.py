6dc5fed5fc7ce1b44559bb09fdf24f92f621b131
Ladipo Ipadeola <rileydrizzy@hotmail.com>
2023-08-01 03:09:15 +0100

    updates

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+"""doc
+
+"""
+
+from pathlib import Path
+from loguru import logger
+
+Format_style = "{time:YYYY_MM_DD HH:mm:ss} | {level} | {module}: {function}{line} - {message}"
+
+log_dir = 'logs'
+log_filepath = Path(log_dir, "running_logs.log")
+Path.mkdir(log_dir, exist_ok= True)
+
+#os.makedirs(log_dir, exist_ok= True)
+
+logger.add(log_filepath,
+    format= Format_style,
+    level= "INFO",)

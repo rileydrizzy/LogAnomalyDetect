9ec522ebbac99d541a996f0931eca0b9d58c0b69
Ladipo Ipadeola <rileydrizzy@hotmail.com>
2023-08-25 04:11:19 +0100

    miscellaneous

--- development/devscript.py
+++ development/devscript.py
@@ -46,3 +46,77 @@ print(f"Precision: {precision:.2f}")
 print(f"Recall: {recall:.2f}")
 print(f"F1-Score: {f1:.2f}")
 print(f"AUC-ROC: {roc_auc:.2f}")
+
+
+"""doc
+"""
+from functools import partial
+
+import tensorflow as tf
+
+
+def build_model(vocab_size, embed_dim, Sequnce_length):
+    """1DCNN doc
+
+    Parameters
+    ----------
+    file_path : str
+
+    Returns
+    -------
+    model : object
+        model
+    """
+
+    input_ = tf.keras.layers.Input(shape=(Sequnce_length,))
+    embedding_layer = tf.keras.layers.Embedding(input_dim=vocab_size, 
+                                                output_dim=embed_dim, mask_zero= True)
+    conv1D = tf.keras.layers.Conv1D(filters=10, kernel_size= 2)
+    pool = tf.keras.layers.MaxPool1D()
+    flatten = tf.keras.layers.GlobalAveragePooling1D()
+    drop1 = tf.keras.layers.Dropout(0.5)
+    dense_layer = tf.keras.layers.Dense(units =100, activation='relu')
+    drop2 = tf.keras.layers.Dropout(0.5)
+    output_layer = tf.keras.layers.Dense(1,activation='sigmoid')
+
+    model = tf.keras.Sequential([input,embedding_layer,conv1D,pool,flatten,drop1,dense_layer,drop2,output_layer])
+    return model
+
+
+
+# !Alerts
+
+
+"""doc
+"""
+
+import mlflow
+import tensorflow as tf
+
+from utils.utils import get_dataset, get_tokenizer, set_seed, tracking
+
+# TODO SET SEED
+set_seed()
+# TODO LOSS, OPTIM
+LOSS = 'temp'
+OPTIM = 'temp'
+tensorb = tf.keras.callbacks.TensorBoard()
+callback_list = ''
+experiment_id = tracking('TEST RUN')
+
+# ! START HERE
+train_data = get_dataset(file_path='data',shuffle= True)
+valid_data = get_dataset(file_path='data')
+tokenizer, vocab_size = get_tokenizer(dataset=train_data)
+
+# TODO Model is been imported
+model_name = '1DCNN'
+main_model = tf.keras.Sequential([tf.keras.layers.Dense(10),
+                                  tf.keras.layers.Dense(1)])
+
+main_model.compile(loss=LOSS, optimizer=OPTIM, metrics=["f1_score"])
+
+mlflow.tensorflow.autolog(log_datasets=False,)
+with mlflow.start_run(run_name=model_name, experiment_id=experiment_id,):
+    # TODO Setup distributed training
+    # # TODO Setup train_time

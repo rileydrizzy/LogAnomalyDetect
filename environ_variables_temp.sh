cef694cc8585bf8b82ee8df823330cb541fb37a0
rileydrizzy <rileydrizzy@hotmail.com>
2024-01-01 22:42:09 +0100

    [add] Added Kaggle username and key

--- environ_variables_temp.sh
+++ environ_variables_temp.sh
@@ -1,6 +1,9 @@
 # replace placeholders and rename this file to `environ_variables.sh`
 #!/bin/
 #./environ_variables.sh
+export KAGGLE_USERNAME=username
+export KAGGLE_KEY=xxxxxxxxxxxxxx
+
 export MLFLOW_TRACKING_URI=xxxxxxxxxxxxxx
 export MLFLOW_TRACKING_USERNAME=xxxxxxxxxxxxxx
 export MLFLOW_TRACKING_PASSWORD=xxxxxxxxxxxxxx
